cda52b9f23c31054efab5c4093f8f27dd46e8305
micScofield <sanyamjaino98@gmail.com>
2021-04-19 18:28:46 +0530

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+const headerLogo = document.getElementById('header-logo')
+headerLogo.addEventListener('click', () => {
+    location.href = '/dist/index.html'
+})
+
+const item1 = document.getElementById('item1').addEventListener('click', () => {
+    window.open('https://afternoon-oasis-20652.herokuapp.com/', '_blank')
+})
+
+const item2 = document.getElementById('item2').addEventListener('click', () => {
+    window.open('https://react-my-burger-61bad.web.app/', '_blank')
+})
+
+const item3 = document.getElementById('item3').addEventListener('click', () => {
+    window.open('https://job-portal-f0633.web.app/', '_blank')
+})
+
+const item4 = document.getElementById('item4').addEventListener('click', () => {
+    window.open('https://wowlabz-todo.web.app/', '_blank')
+})
+
+const item5 = document.getElementById('item5').addEventListener('click', () => {
+    window.open('https://custom-forms-98973.web.app/', '_blank')
+})
+
+const item6 = document.getElementById('item6').addEventListener('click', () => {
+    window.open('https://serene-perlman-5ac20c.netlify.app/', '_blank')
+})
